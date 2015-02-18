f1cf4c5b4efcc4098bcf2c1fc00650771a8381b7
zuoyizhang <zyizhang@indiana.edu>
2015-02-18 16:24:36 -0500

    Update test.sql

--- inst/sql/oracle/test.sql
+++ inst/sql/oracle/test.sql
@@ -1,2 +1,2 @@
-{DEFAULT @selected_value = 2000000603568810}
+{DEFAULT @selected_value = 2000002147483568}
 INSERT INTO zyizhang.result SELECT   * FROM OMOPV4_de.person where person_id = @selected_value;
