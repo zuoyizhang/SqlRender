4d0bfd20b330876971440264e90de9497c6c1613
zuoyizhang <zyizhang@indiana.edu>
2015-02-19 15:04:36 -0500

    Update test.sql

--- inst/sql/sql_server/test.sql
+++ inst/sql/sql_server/test.sql
@@ -1,2 +1,2 @@
-{DEFAULT @id=person_id} {DEFAULT @selected_value = 2000000603568810}
-SELECT * FROM person INTO result where @id = @selected_value;
+{DEFAULT @selected_value = 1}
+SELECT * FROM table INTO result where x = @selected_value;
